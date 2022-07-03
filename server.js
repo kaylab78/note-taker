4b20b715ec5df15fc2a0301805676b895793e781
kaylab78 <kaylabackus78@gmail.com>
2022-07-03 14:22:31 -0500

    Create new note and send POST request

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { notes } = require('./data/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/notes.html"));
+// });
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
 
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
@@ -17,8 +29,34 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
+// Read the db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
-    // res.json(notes);
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
@@ -26,53 +64,28 @@ app.get('/api/notes', (req, res) => {
     res.json(results);
 });
 
-/*
-app.post('/api/notes', (req, res) => {
-  // set id based on what the next index of the array will be
-    // req.body.id = notes.length.toString();
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
-  // if any data in req.body is incorrect, send 400 error back
-    // if (!validateNote(req.body)) {
-    // res.status(400).send("This note does not exist.");
-    // } else {
-    // const notes = createNewNote(req.body, notes);
-    // res.json(notes);
-    // }
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('Your note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 })
-*/
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
-
-// middleware
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/notes.html"));
-// });
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/index.html"))
-// });
-
-// function filterByQuery(query, notesArray) {
-//     let filteredResults = notesArray;
-//     if (query.title) {
-//         filteredResults = filteredResults.filter(note => note.title === note.title);
-//     }
-//     if (query.text) {
-//         filteredResults = filteredResults.filter(note => note.text === note.text);
-//     }
-//     return filteredResults;
-// }
-
-// app.get('/api/notes', (req,res) => {
-//     let results = notes;
-//     if (req.query) {
-//         results = filterByQuery(req.query, results);
-//     }
-//     res.json(results);
-//     Read the db.json file and return all saved notes as JSON
-// });
